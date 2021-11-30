1feeb7773dc93838018ff729500bc0874455cd7d
yui0109 <yui.gangan@icloud.com>
2021-11-30 14:40:02 +0000

    blog list

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,4 +13,4 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/','PostController@index');
