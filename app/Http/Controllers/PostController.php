2b05ee5c3384b603cafc23b80b20c91b6833dd29
yui0109 <yui.gangan@icloud.com>
2021-12-01 09:03:27 +0000

    blog create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,10 @@ class PostController extends Controller
     {
         return view('show')->with(['post' => $post]);
     }
+    
+    
+    public function create()
+    {
+        return view('create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}','PostController@show');
 
 
